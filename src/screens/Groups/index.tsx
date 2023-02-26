0324bc70c776957f6bfe4bf1024bcfae707768ff
Bruno Santos <brunojsfla@gmail.com>
2023-02-26 11:35:26 -0300

    feat: instalação e configuração do styled-component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text } from "react-native";
+
+import * as S from "./styles";
 
 export function Groups() {
   return (
-    <View style={styles.container}>
+    <S.Container>
       <Text>Groups</Text>
       <StatusBar style="auto" />
-    </View>
+    </S.Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
