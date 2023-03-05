6d1a2a0159bdeaeed335848e066112f6635ad472
Bruno Santos <brunojsfla@gmail.com>
2023-03-05 17:34:57 -0300

    feat: configuração do tema da aplicação

--- App.tsx
+++ App.tsx
@@ -1,5 +1,26 @@
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
+import theme from "./src/theme";
+
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={"light-content"}
+        translucent
+        backgroundColor="transparent"
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
